8de0232693256eabf27babffedbb7de7a2ac5c6d
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-05-18 19:20:30 -0700

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -178,31 +178,85 @@ SELECT players.player_name,
 -- Write a query that performs an inner join between the tutorial.crunchbase_acquisitions table 
 -- and the tutorial.crunchbase_companies table, but instead of listing individual rows, 
 -- count the number of non-null rows in each table.
-
+SELECT COUNT(companies.permalink) AS companies_rowcount,
+       COUNT(acquisitions.company_permalink) AS acquisitions_rowcount
+  FROM tutorial.crunchbase_companies companies
+  JOIN tutorial.crunchbase_acquisitions acquisitions
+    ON companies.permalink = acquisitions.company_permalink;
+    
 -- Modify the query above to be a LEFT JOIN. Note the difference in results.
-
+SELECT COUNT(companies.permalink) AS companies_rowcount,
+       COUNT(acquisitions.company_permalink) AS acquisitions_rowcount
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_acquisitions acquisitions
+    ON companies.permalink = acquisitions.company_permalink;
+    
 -- Count the number of unique companies (don't double-count companies) and unique acquired companies by state. 
 -- Do not include results for which there is no state data, 
 -- and order by the number of acquired companies from highest to lowest.
+SELECT companies.state_code,
+       COUNT(DISTINCT companies.permalink) AS unique_companies,
+       COUNT(DISTINCT acquisitions.company_permalink) AS unique_companies_acquired
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_acquisitions acquisitions
+    ON companies.permalink = acquisitions.company_permalink
+ WHERE companies.state_code IS NOT NULL
+ GROUP BY 1
+ ORDER BY 3 DESC;
 
 
 -- SQL RIGHT JOIN
 -- Rewrite the previous practice query in which you counted total and acquired companies by state, 
 -- but with a RIGHT JOIN instead of a LEFT JOIN. The goal is to produce the exact same results.
+SELECT companies.state_code,
+       COUNT(DISTINCT companies.permalink) AS unique_companies,
+       COUNT(DISTINCT acquisitions.company_permalink) AS acquired_companies
+  FROM tutorial.crunchbase_acquisitions acquisitions
+ RIGHT JOIN tutorial.crunchbase_companies companies
+    ON companies.permalink = acquisitions.company_permalink
+ WHERE companies.state_code IS NOT NULL
+ GROUP BY 1
+ ORDER BY 3 DESC;
 
 
 -- SQL Joins Using WHERE or ON
 -- Write a query that shows a company's name, "status" (found in the Companies table), 
 -- and the number of unique investors in that company. 
 -- Order by the number of investors from most to fewest. Limit to only companies in the state of New York.
-
+SELECT companies.name AS company_name,
+       companies.status,
+       COUNT(DISTINCT investments.investor_name) AS unqiue_investors
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments investments
+    ON companies.permalink = investments.company_permalink
+ WHERE companies.state_code = 'NY'
+ GROUP BY 1,2
+ ORDER BY 3 DESC;
+ 
 -- Write a query that lists investors based on the number of companies in which they are invested. 
 -- Include a row for companies with no investor, and order from most companies to least.
-
+SELECT CASE WHEN investments.investor_name IS NULL THEN 'No Investors'
+            ELSE investments.investor_name END AS investor,
+       COUNT(DISTINCT companies.permalink) AS companies_invested_in
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments investments
+    ON companies.permalink = investments.company_permalink
+ GROUP BY 1
+ ORDER BY 2 DESC;
+ 
 
 -- SQL FULL OUTER JOIN
 -- Write a query that joins tutorial.crunchbase_companies and tutorial.crunchbase_investments_part1 using a FULL JOIN. 
 -- Count up the number of rows that are matched/unmatched as in the example above.
+SELECT COUNT(CASE WHEN companies.permalink IS NOT NULL AND investments.company_permalink IS NULL
+                  THEN companies.permalink ELSE NULL END) AS companies_only,
+       COUNT(CASE WHEN companies.permalink IS NOT NULL AND investments.company_permalink IS NOT NULL
+                  THEN companies.permalink ELSE NULL END) AS both_tables,
+       COUNT(CASE WHEN companies.permalink IS NULL AND investments.company_permalink IS NOT NULL
+                  THEN investments.company_permalink ELSE NULL END) AS investments_only
+  FROM tutorial.crunchbase_companies companies
+  FULL JOIN tutorial.crunchbase_investments_part1 investments
+       ON companies.permalink = investments.company_permalink;
 
 
 -- SQL UNION
@@ -210,6 +264,32 @@ SELECT players.player_name,
 -- Filter the first dataset to only companies with names that start with the letter "T", 
 -- and filter the second to companies with names starting with "M" (both not case-sensitive). 
 -- Only include the company_permalink, company_name, and investor_name columns.
+SELECT company_permalink, company_name, investor_name
+  FROM tutorial.crunchbase_investments_part1
+ WHERE company_name ILIKE 'T%'
+ 
+ UNION ALL
 
+SELECT company_permalink, company_name, investor_name
+  FROM tutorial.crunchbase_investments_part2
+ WHERE company_name ILIKE 'M%';
+ 
 -- Write a query that shows 3 columns. The first indicates which dataset (part 1 or 2) the data comes from, 
 -- the second shows company status, and the third is a count of the number of investors.
+SELECT 'investments_part1' AS dataset_name,
+       companies.status,
+       COUNT(DISTINCT investments.investor_permalink) AS investors
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments_part1 investments
+    ON companies.permalink = investments.company_permalink
+ GROUP BY 1,2
+
+ UNION ALL
+ 
+ SELECT 'investments_part2' AS dataset_name,
+       companies.status,
+       COUNT(DISTINCT investments.investor_permalink) AS investors
+  FROM tutorial.crunchbase_companies companies
+  LEFT JOIN tutorial.crunchbase_investments_part2 investments
+    ON companies.permalink = investments.company_permalink
+ GROUP BY 1,2;
